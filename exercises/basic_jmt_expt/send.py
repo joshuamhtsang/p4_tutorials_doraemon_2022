0bac40e1ff140708da2c8ef43894314db673e086
Joshua Tsang <joshua.mh.tsang@gmail.com>
2023-09-29 15:38:28 +0100

    make doraemon layer in scapy.  Now need to write some P4?

--- exercises/basic_jmt_expt/send.py
+++ exercises/basic_jmt_expt/send.py
@@ -3,8 +3,28 @@ import random
 import socket
 import sys
 
-from scapy.all import IP, TCP, Ether, get_if_hwaddr, get_if_list, sendp
+from scapy.all import (
+    IP, 
+    TCP, 
+    Ether, 
+    get_if_hwaddr, 
+    get_if_list, 
+    sendp,
+    Packet,
+    IntField,
+    bind_layers
+    )
 
+TYPE_MYDORAEMON = 0x1212
+
+
+class MyDoraemon(Packet):
+    name = "MyDoraemon"
+    fields_desc = [
+        IntField("dorayaki", 0) # Start with 0 dorayaki
+    ]
+
+bind_layers(Ether, MyDoraemon, type=TYPE_MYDORAEMON)
 
 def get_if():
     ifs=get_if_list()
@@ -29,8 +49,9 @@ def main():
 
     print("sending on interface %s to %s" % (iface, str(addr)))
     pkt =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
-    pkt = pkt /IP(dst=addr) / TCP(dport=1234, sport=random.randint(49152,65535)) / sys.argv[2]
+    pkt = pkt / IP(dst=addr) / TCP(dport=1234, sport=random.randint(49152,65535)) / MyDoraemon(dorayaki=5) / sys.argv[2]
     pkt.show2()
+    pkt.show()
     sendp(pkt, iface=iface, verbose=False)
 
 
