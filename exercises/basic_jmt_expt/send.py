7a88e30b96a32d3e895957fa81f3555460c39d94
Joshua Tsang <joshua.tsang@soundmouse.com>
2023-10-03 15:03:29 +0100

    setting to zero initial dorayaki

--- exercises/basic_jmt_expt/send.py
+++ exercises/basic_jmt_expt/send.py
@@ -36,7 +36,7 @@ def main():
 
     print("sending on interface %s to %s" % (iface, str(addr)))
     pkt =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
-    pkt = pkt / IP(dst=addr) / TCP(dport=1234, sport=random.randint(49152,65535)) / MyDoraemon(dorayaki=105) / sys.argv[2]
+    pkt = pkt / IP(dst=addr) / TCP(dport=1234, sport=random.randint(49152,65535)) / MyDoraemon(dorayaki=0) / sys.argv[2]
     print(pkt.show2())
     print(pkt.show())
     sendp(pkt, iface=iface, verbose=False)
